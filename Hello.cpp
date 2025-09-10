23393450b14db4b81bc7f9d75a2489f8dda12717
mahmoodakhtar60 <makhtar1@student.ohlone.edu>
2025-09-09 18:54:32 -0700

    final lab4 part 1

--- Hello.cpp
+++ Hello.cpp
@@ -1,29 +1,47 @@
 
-
+#include <cmath>
 #include <iostream>
 #include <string>
+#include<iomanip>
 using namespace std;
-int totalseconds, seconds, minutes, hours;
+int total;
+string name;
+int score;
+int counter=0;
+string grade;
+double average;
 int main()
 {
-    cout<<"enter total seconds";
-    cin>>totalseconds;
-    
-    seconds=totalseconds%60;
-    totalseconds-=seconds;
-    totalseconds/=60;
-    minutes=totalseconds%60;
-    totalseconds-=minutes;
-    hours=totalseconds/60;
+cout<<"Enter student fulls name"<<endl;
+getline(cin,name);
+cout<<"Enter student test scores"<<endl;
+while(cin>>score && score != -1){
+    total += score;
+    counter++;
+}
+ counter > 0 ? average=static_cast<double>(total)/counter : 0;
 
-    cout<<"Hours"<<endl;
-    cout<<hours<<endl;
-    cout<<"minutes"<<endl;
-    cout<<minutes<<endl;
-    cout<<"seconds"<<endl;
-    cout<<seconds<<endl;
-    return 0;
+switch(static_cast<int>(average)){
+    case 90 ... 100:
+        grade = "A";
+        break;
+    case 80 ... 89:
+        grade = "B";
+        break;
+    case 70 ... 79:
+        grade = "C";
+        break;
+    case 60 ... 69:
+        grade = "D";
+        break;
+    default:
+        grade = "F";
+}
 
+cout<< setw(10) << "Name" << setw(10) << "Average" << setw(10) << "Grade" << endl;;
+cout<< setw(10) << name << setw(10) << average << setw(10) << grade << endl;;
+
+return 0;
 }
 
 
